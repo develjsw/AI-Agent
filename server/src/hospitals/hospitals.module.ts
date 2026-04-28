105d1ac51e3c41bc08974c79bcee8cdfc3c855fb
jsw <develjsw@gmail.com>
2026-04-28 22:29:16 +0900

    init(healthcare-voice-agent): 프로젝트 초기화

--- server/src/hospitals/hospitals.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HospitalsController } from './hospitals.controller';
-import { HospitalsService } from './hospitals.service';
-
-@Module({
-  controllers: [HospitalsController],
-  providers: [HospitalsService],
-})
-export class HospitalsModule {}
